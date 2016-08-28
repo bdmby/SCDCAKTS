9e5f585c69bf5c7211b6e1b73564602ea25499c1
Dzmitry Brukhanchyk <bdm@tut.by>
2016-08-28 17:42:57 +0300

    Рефакторинг

--- SCDCAkts.dpr
+++ SCDCAkts.dpr
@@ -3,9 +3,10 @@ program SCDCAkts;
 uses
   Vcl.Forms,
   SCDCAkts_MainFormUnit in 'SCDCAkts_MainFormUnit.pas' {SCDCAkts_MainForm},
-  SCDCAkts_CustomerSettingFormUnit in 'SCDCAkts_CustomerSettingFormUnit.pas' {CustomerSettingForm},
-  SCDCAkts_DataModuleUnit in 'SCDCAkts_DataModuleUnit.pas' {SCDCAktsDataModule: TDataModule},
-  SCDCAkts_SubjectSettingFormUnit in 'SCDCAkts_SubjectSettingFormUnit.pas' {SCDCAkts_SubjectSettingForm};
+  SCDCAkts_CustomerSettingFormUnit in 'SCDCAkts_CustomerSettingFormUnit.pas' {SCDCAkts_CustomerSettingForm},
+  SCDCAkts_DataModuleUnit in 'SCDCAkts_DataModuleUnit.pas' {SCDCAkts_DataModule: TDataModule},
+  SCDCAkts_SubjectSettingFormUnit in 'SCDCAkts_SubjectSettingFormUnit.pas' {SCDCAkts_SubjectSettingForm},
+  SCDCAkts_ClientSettingFormUnit in 'SCDCAkts_ClientSettingFormUnit.pas' {SCDCAkts_ClientSettingForm};
 
 {$R *.res}
 
@@ -13,6 +14,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TSCDCAkts_MainForm, SCDCAkts_MainForm);
-  Application.CreateForm(TSCDCAktsDataModule, SCDCAktsDataModule);
+  Application.CreateForm(TSCDCAkts_DataModule, SCDCAkts_DataModule);
+  Application.CreateForm(TSCDCAkts_ClientSettingForm, SCDCAkts_ClientSettingForm);
   Application.Run;
 end.
